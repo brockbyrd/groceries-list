dde05bdbe19788e4640bc73ed21233b9739dd975
brockbyrd <brockbyrdd@gmail.com>
2021-03-10 11:45:18 -0600

    styles input

--- src/components/GroceryInput.js
+++ src/components/GroceryInput.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 function GroceryInput() {
     const [item, setItem] = useState('');
@@ -7,27 +12,44 @@ function GroceryInput() {
     const [count, setCount] = useState(0);
 
         return (
-            <div>
-                <input
-                    type="text"
-                    className="grocery_input"
-                    placeholder="Grocery Item"
-                    value={item}
-                    onChange={(event) => setItem(event.target.value)}
-                />
-                <input
-                    type="text"
-                    className="grocery_input"
-                    placeholder="Grocery Brand"
-                    value={brand}
-                    onChange={(event) => setBrand(event.target.value)}
-                />
+            <Container>
+                <Form>
+                    <Form.Row className="justify-content-md-center">
 
-                <Button variant="outline-dark" onClick={() => setCount(count + 1)}> + </Button>
-                <p className="counter">{count}</p>
-                <Button variant="outline-dark" onClick={() => setCount(count - 1)}> - </Button>
-                <input type="submit" value="Submit" />
-            </div>
+                        <Col>
+                            <Form.Control
+                                type="text"
+                                className="grocery_input"
+                                placeholder="Grocery Item"
+                                value={item}
+                                onChange={(event) => setItem(event.target.value)}
+                            />
+                        </Col>
+
+                        <Col>
+                            <Form.Control
+                                type="text"
+                                className="grocery_input"
+                                placeholder="Grocery Brand"
+                                value={brand}
+                                onChange={(event) => setBrand(event.target.value)}
+                            />
+                        </Col>
+
+                        <Col>
+                                <ButtonGroup size="sm">
+                                    <Button variant="outline-dark" onClick={() => setCount(count - 1)}>-</Button>
+                                        <h3 className="counter">{count}</h3>
+                                    <Button variant="outline-dark" onClick={() => setCount(count + 1)}>+</Button>
+                                </ButtonGroup>
+                        </Col>
+
+                        <Col>
+                            <input type="submit" value="Submit" />
+                        </Col>
+                    </Form.Row>
+                </Form>
+            </Container>
         )
 }
 
